333cdcd5e695c0c030fd5708f218bdd449e50e58
Emile ASTIH <emileastih1@gmail.com>
2024-01-11 16:43:35 +0100

    new feature

--- src/main/java/com/baeldung/springbootsimple/TomcatController.java
+++ src/main/java/com/baeldung/springbootsimple/TomcatController.java
@@ -10,8 +10,15 @@ public class TomcatController {
 
     @GetMapping(value = "/hello")
     public Collection<String> sayHello() {
-        return IntStream.range(0, 15)
+        return IntStream.range(0, 10)
                 .mapToObj(i -> "Hello number " + i)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/newFeature")
+    public Collection<String> newFeature() {
+        return IntStream.range(0, 15)
+                .mapToObj(i -> "New Feature: " + i)
                 .collect(Collectors.toList());
     }
 
